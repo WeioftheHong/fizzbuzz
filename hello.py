f7c94b87e48eb1831f0642c2a8830b45e82bd5a7
weihong <wei-hong@hotmail.com>
2017-08-02 10:29:41 +1000

    Implemented fizzbuzz function

--- hello.py
+++ hello.py
@@ -1,4 +1,25 @@
 # hello.py
 # says 'Hello, git!'
+# also does fizzBuzz function
+
+def fizzBuzzIterative(start, end):
+    for i in range(start, end):
+        fizzBuzzPrint(i)
+
+def fizzBuzzPrint(n):
+    mod3 = not n % 3
+    mod5 = not n % 5
+    if (not mod3 and not mod5):
+        print(n)
+    elif (mod3 and mod5):
+        print("Fizz Buzz")
+    elif (mod3):
+        print("Fizz ")
+    elif (mod5):
+        print("Buzz ")
 
 print("'Hello, git!'")
+
+n = int(input("Input the final FizzBuzz number "))
+
+fizzBuzzIterative(1, n + 1)
